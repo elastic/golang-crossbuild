713f77de803735e977c8bd9970343d2436d42233
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2022-05-13 15:22:14 +0200

    fix: force env vars

--- .ci/scripts/buildx.sh
+++ .ci/scripts/buildx.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+set -e
+set +x
 
-export DOCKER_USER=${DOCKER_USER:?-"Missing environment variable"}
-export DOCKER_PASSWORD=${DOCKER_PASSWORD:?-"Missing environment variable"}
-export DOCKER_REGISTRY=${DOCKER_REGISTRY:-"docker.elastic.co"}
+DOCKER_USER=${DOCKER_USER:?-"Missing environment variable"}
+DOCKER_PASSWORD=${DOCKER_PASSWORD:?-"Missing environment variable"}
+DOCKER_REGISTRY=${DOCKER_REGISTRY:-"docker.elastic.co"}
 
 docker run --rm -t \
-    -e DOCKER_USER -e DOCKER_PASSWORD -e DOCKER_REGISTRY \
+    -e DOCKER_USER=${DOCKER_USER} -e DOCKER_PASSWORD=${DOCKER_PASSWORD} -e DOCKER_REGISTRY=${DOCKER_REGISTRY} \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v $(pwd):/app -w /app \
     docker.elastic.co/observability-ci/dind-buildx:latest """
@@ -14,7 +16,8 @@ echo 'Create builder'
 docker buildx create --name multibuilder
 docker buildx use multibuilder
 docker buildx inspect --bootstrap
-echo 'Build Docker image'
+echo 'Docker login ${DOCKER_REGISTRY}'
 docker login ${DOCKER_REGISTRY}
+echo 'Build Docker image'
 docker buildx build --platform linux/amd64,linux/arm64 --push $*
 """
