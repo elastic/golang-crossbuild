038e7a812cc0227d8383fc81adbc654ac515c588
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2022-05-13 15:58:23 +0200

    fix: install binfmt Docker images

--- .ci/scripts/buildx.sh
+++ .ci/scripts/buildx.sh
@@ -6,6 +6,7 @@ DOCKER_USER=${DOCKER_USER:?-"Missing environment variable"}
 DOCKER_PASSWORD=${DOCKER_PASSWORD:?-"Missing environment variable"}
 DOCKER_REGISTRY=${DOCKER_REGISTRY:-"docker.elastic.co"}
 
+docker run --privileged --rm tonistiigi/binfmt --install all
 docker pull --platform=linux/arm64 --platform=linux/amd64  moby/buildkit:buildx-stable-1
 
 docker run --rm -t \
