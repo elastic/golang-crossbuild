b10b76bee7021607fe8695f4228e95ab99c97ddf
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-05-18 14:27:39 +0100

    fix go version

--- go/base/install-go.sh
+++ go/base/install-go.sh
@@ -2,9 +2,9 @@
 # This script install the Go version correct for each architecture.
 set -e
 
-GOLANG_VERSION=1.18.1
+GOLANG_VERSION=1.18.2
 GOLANG_DOWNLOAD_URL=https://golang.org/dl/go$GOLANG_VERSION.linux-amd64.tar.gz
-GOLANG_DOWNLOAD_SHA256=b3b815f47ababac13810fc6021eb73d65478e0b2db4b09d348eefad9581a2334
+GOLANG_DOWNLOAD_SHA256=e54bec97a1a5d230fc2f9ad0880fcbabb5888f30ed9666eca4a91c5a32e86cbc
 
 GO_TAR_FILE=/tmp/golang.tar.gz
 
