71855eec663f4a7f9ed5cdf5a704a591f9dec4f6
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2022-05-26 10:30:33 +0200

    test: build and push

--- .ci/llvm-apple.groovy
+++ .ci/llvm-apple.groovy
@@ -53,9 +53,9 @@ pipeline {
     stage('Build amd64') {
       when {
         anyOf {
-        expression {
-          return  dir(BASE_DIR){isGitRegionMatch(patterns: ['^\\.ci/llvm-apple.groovy', '^/go/llvm-apple'], shouldMatchAll: false)}
-        }
+          expression {
+            return  dir(BASE_DIR){isGitRegionMatch(patterns: ['^\\.ci/llvm-apple.groovy', '^/go/llvm-apple'], shouldMatchAll: false)}
+          }
           expression { return isUserTrigger() }
         }
       }
@@ -75,54 +75,11 @@ pipeline {
           stage('Build'){
             environment {
                 MAKEFILE = "go/llvm-apple"
-                TAG_EXTENSION = "-debian${env.DEBIAN_VERSION}"
-                BUILDPLATFORM = 'linux/amd64'
+                TAG_EXTENSION = "-debian${env.DEBIAN_VERSION}-${AGENT_LABELS == 'ubuntu-2204-aarch64' ? 'arm64' : 'amd64' }"
             }
             options { skipDefaultCheckout() }
             steps {
               stageStatusCache(id: "Build amd64 ${MAKEFILE}") {
-                whenTrue(isPR()){
-                  setEnvVar("REPOSITORY", "${env.STAGING_IMAGE}")
-                }
-                withGithubNotify(context: "Build ${MAKEFILE}") {
-                  deleteDir()
-                  unstash 'source'
-                  //FIXME disable to test ARM64 only buildImages()
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-    /*
-    stage('Build arm64') {
-      when {
-        anyOf {
-        expression {
-          return  dir(BASE_DIR){isGitRegionMatch(patterns: ['^\\.ci/llvm-apple.groovy', '^/go/llvm-apple'], shouldMatchAll: false)}
-        }
-          expression { return isUserTrigger() }
-        }
-      }
-      matrix {
-        agent { label 'ubuntu-2204-aarch64' }
-        axes {
-          axis {
-            name 'DEBIAN_VERSION'
-            values '10', '11'
-          }
-        }
-        stages {
-          stage('Build'){
-            environment {
-                MAKEFILE = "go/llvm-apple"
-                TAG_EXTENSION = "-debian${env.DEBIAN_VERSION}"
-                BUILDPLATFORM ='linux/arm64'
-            }
-            options { skipDefaultCheckout() }
-            steps {
-              stageStatusCache(id: "Build arm64 ${MAKEFILE}") {
                 whenTrue(isPR()){
                   setEnvVar("REPOSITORY", "${env.STAGING_IMAGE}")
                 }
@@ -137,7 +94,6 @@ pipeline {
         }
       }
     }
-    */
   }
 }
 
@@ -150,6 +106,15 @@ def buildImages() {
         }
       sh(label: 'list Docker images', script: 'docker images --format "table {{.Repository}}:{{.Tag}}\t{{.Size}}" --filter=reference="*/*/golang-crossbuild:llvm-apple*"')
       }
+    }
+  }
+}
+
+def publishImages() {
+  dockerLogin(secret: "${env.DOCKER_REGISTRY_SECRET}", registry: "${env.REGISTRY}")
+  dir("${env.BASE_DIR}") {
+    retryWithSleep(retries: 3, seconds: 15, backoff: true) {
+      sh(label: "push docker image to ${env.REPOSITORY}", script: "make -C ${MAKEFILE} push")
     }
   }
 }
