a29740c59dd3478cae6450cb99f97811fb6a3e77
Victor Martinez <victormartinezrubio@gmail.com>
2022-06-28 18:58:09 +0100

    Update go/base/install-go.sh

--- go/base/install-go.sh
+++ go/base/install-go.sh
@@ -4,17 +4,10 @@ set -e
 
 ## These variables are automatically bumped.
 ## If you change their name please change .ci/bump-go-release-version.sh
-<<<<<<< HEAD
 GOLANG_VERSION=1.18.2
 GOLANG_DOWNLOAD_URL=https://golang.org/dl/go$GOLANG_VERSION.linux-amd64.tar.gz
 GOLANG_DOWNLOAD_SHA256_AMD=e54bec97a1a5d230fc2f9ad0880fcbabb5888f30ed9666eca4a91c5a32e86cbc
 GOLANG_DOWNLOAD_SHA256_ARM=fc4ad28d0501eaa9c9d6190de3888c9d44d8b5fb02183ce4ae93713f67b8a35b
-=======
-GOLANG_VERSION=1.18.1
-GOLANG_DOWNLOAD_URL=https://golang.org/dl/go$GOLANG_VERSION.linux-amd64.tar.gz
-GOLANG_DOWNLOAD_SHA256_AMD=b3b815f47ababac13810fc6021eb73d65478e0b2db4b09d348eefad9581a2334
-GOLANG_DOWNLOAD_SHA256_ARM=56a91851c97fb4697077abbca38860f735c32b38993ff79b088dac46e4735633
->>>>>>> c41256f (fix auto-bump (#204))
 
 GO_TAR_FILE=/tmp/golang.tar.gz
 
