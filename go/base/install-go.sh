1424b067c4f569766a023a7e042a6ba24e8debf5
apmmachine <58790750+apmmachine@users.noreply.github.com>
2022-08-30 12:16:25 -0400

    [Automation] Update go release version to 1.19 (#223)
    Co-authored-by: apmmachine <infra-root-apmmachine@elastic.co>

--- go/base/install-go.sh
+++ go/base/install-go.sh
@@ -4,10 +4,10 @@ set -e
 
 ## These variables are automatically bumped.
 ## If you change their name please change .ci/bump-go-release-version.sh
-GOLANG_VERSION=1.18.5
+GOLANG_VERSION=1.19
 GOLANG_DOWNLOAD_URL=https://golang.org/dl/go$GOLANG_VERSION.linux-amd64.tar.gz
-GOLANG_DOWNLOAD_SHA256_AMD=9e5de37f9c49942c601b191ac5fba404b868bfc21d446d6960acc12283d6e5f2
-GOLANG_DOWNLOAD_SHA256_ARM=006f6622718212363fa1ff004a6ab4d87bbbe772ec5631bab7cac10be346e4f1
+GOLANG_DOWNLOAD_SHA256_AMD=464b6b66591f6cf055bc5df90a9750bf5fbc9d038722bb84a9d56a2bea974be6
+GOLANG_DOWNLOAD_SHA256_ARM=efa97fac9574fc6ef6c9ff3e3758fb85f1439b046573bf434cccb5e012bd00c8
 
 GO_TAR_FILE=/tmp/golang.tar.gz
 
