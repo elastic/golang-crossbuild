373715b825f8db170eba151ebf47a9b275fe7752
apmmachine <58790750+apmmachine@users.noreply.github.com>
2022-09-12 05:22:48 -0400

    [Automation] Update go release version to 1.18.6 (#234)
    Co-authored-by: apmmachine <infra-root-apmmachine@elastic.co>

--- go/base/install-go.sh
+++ go/base/install-go.sh
@@ -4,10 +4,10 @@ set -e
 
 ## These variables are automatically bumped.
 ## If you change their name please change .ci/bump-go-release-version.sh
-GOLANG_VERSION=1.18.5
+GOLANG_VERSION=1.18.6
 GOLANG_DOWNLOAD_URL=https://golang.org/dl/go$GOLANG_VERSION.linux-amd64.tar.gz
-GOLANG_DOWNLOAD_SHA256_AMD=9e5de37f9c49942c601b191ac5fba404b868bfc21d446d6960acc12283d6e5f2
-GOLANG_DOWNLOAD_SHA256_ARM=006f6622718212363fa1ff004a6ab4d87bbbe772ec5631bab7cac10be346e4f1
+GOLANG_DOWNLOAD_SHA256_AMD=bb05f179a773fed60c6a454a24141aaa7e71edfd0f2d465ad610a3b8f1dc7fe8
+GOLANG_DOWNLOAD_SHA256_ARM=838ffa94158125f16e4aa667ee4f6b499ea57e3e35a7e2517ad357ea06714691
 
 GO_TAR_FILE=/tmp/golang.tar.gz
 
