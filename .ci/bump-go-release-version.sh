f262b64b260e3482da4aa998d18d5ca83bbccdbf
Victor Martinez <victormartinezrubio@gmail.com>
2023-02-07 09:21:11 +0000

    automate bump golang version (#262)

--- .ci/bump-go-release-version.sh
+++ .ci/bump-go-release-version.sh
@@ -3,7 +3,6 @@
 # Given the Golang release version this script will bump the version.
 #
 # This script is executed by the automation we are putting in place
-# and it requires the git add/commit commands.
 #
 # NOTE: sha256 is retrieved from https://pkg.go.dev/golang.org/x/website/internal/dl?utm_source=godoc
 #
@@ -26,9 +25,7 @@ GOLANG_DOWNLOAD_SHA256_AMD=$(curl -s -L https://golang.org/dl/\?mode\=json | jq 
 
 echo "Update go version ${GO_RELEASE_VERSION}"
 ${SED} -E -e "s#(VERSION[[:space:]]+):= .*#\1:= ${GO_RELEASE_VERSION}#g" "go/Makefile.common"
-git add "go/Makefile.common"
 ${SED} -E -e "s#(GO_VERSION[[:space:]]+)= .*#\1= '${GO_RELEASE_VERSION}'#g" Jenkinsfile
-git add Jenkinsfile
 
 find "go" -type f -name Dockerfile.tmpl -print0 |
     while IFS= read -r -d '' line; do
@@ -38,17 +35,10 @@ find "go" -type f -name Dockerfile.tmpl -print0 |
         else
             ${SED} -E -e "s#(ARG GOLANG_DOWNLOAD_SHA256)=.+#\1=${GOLANG_DOWNLOAD_SHA256_AMD}#g" "$line"
         fi
-        git add "${line}"
     done
 
 if [ -e go/base/install-go.sh ] ; then
     ${SED} -E -e "s#(GOLANG_VERSION)=[0-9]+\.[0-9]+(\.[0-9]+)?#\1=${GO_RELEASE_VERSION}#g" go/base/install-go.sh
     ${SED} -E -e "s#(GOLANG_DOWNLOAD_SHA256_AMD)=.+#\1=${GOLANG_DOWNLOAD_SHA256_AMD}#g" go/base/install-go.sh
     ${SED} -E -e "s#(GOLANG_DOWNLOAD_SHA256_ARM)=.+#\1=${GOLANG_DOWNLOAD_SHA256_ARM}#g" go/base/install-go.sh
-    git add go/base/install-go.sh
 fi
-
-git diff --staged --quiet || git commit -m "[Automation] Update go release version to ${GO_RELEASE_VERSION}"
-git --no-pager log -1
-
-echo "You can now push and create a Pull Request"
