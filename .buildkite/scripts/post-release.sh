02946f4a624ea2fd1079813b93f100e0f5b2649c
Olga Naydyonock <olga.naidjonoka@elastic.co>
2023-11-08 17:29:07 +0200

    Migrate golang-crossbuild pipeline (#326)
    Co-authored-by: Julien Lind <julien.lind@elastic.co>

---
+++ .buildkite/scripts/post-release.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source .buildkite/scripts/common.sh
+
+TAG="v{$1}"
+TAG_EXISTS=$(tag_Exists ${TAG})
+
+set_git_config() {
+    git config user.name "${GITHUB_USERNAME_SECRET}"
+    git config user.email "${GITHUB_EMAIL_SECRET}"
+}
+
+tag_commit() {
+  echo "Tagging commit ${BUILDKITE_COMMIT}"
+  git tag -a -m "${BUILDKITE_COMMIT}" "${TAG}"
+}
+
+git_push_with_auth() {
+  echo "Pushing tag ${TAG}"
+  retry 3 git push https://${GITHUB_USERNAME_SECRET}:${GITHUB_TOKEN_SECRET}@github.com/elastic/golang-crossbuild.git ${TAG}
+}
+
+if [[ "${TAG_EXISTS}" == true ]]; then
+  echo "Tag already exists! Exiting Post-release stage."
+  exit 1
+fi
+
+set_git_config
+tag_commit
+git_push_with_auth
+
+
