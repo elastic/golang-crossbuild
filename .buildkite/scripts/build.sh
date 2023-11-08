48ee2037212ef8baa15c2f0d7bcb92c7102322f8
Olga Naydyonock <olga.naidjonoka@elastic.co>
2023-11-08 17:29:07 +0200

    Migrate golang-crossbuild pipeline (#326)
    Co-authored-by: Julien Lind <julien.lind@elastic.co>
    (cherry picked from commit 02946f4a624ea2fd1079813b93f100e0f5b2649c)

---
+++ .buildkite/scripts/build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source .buildkite/scripts/common.sh
+
+MAKEFILE=${1}
+
+check_is_arm
+
+add_bin_path
+with_go "${GOLANG_VERSION}"
+with_mage
+google_cloud_auth
+
+make -C go -f "${MAKEFILE}" build"${is_arm}" GS_BUCKET_PATH=ingest-buildkite-ci
+docker images --format "table {{.Repository}}:{{.Tag}}\t{{.Size}}" --filter=reference="${STAGING_IMAGE}/golang-crossbuild"
+docker images --format "table {{.Repository}}:{{.Tag}}\t{{.Size}}" --filter=reference="${DOCKER_REGISTRY}/beats-dev/golang-crossbuild"
+
+
