d05c2434b489e02db02dcf1afca6e99f31022825
Olga Naidjonoka <olga_naidjonoka@epam.com>
2023-11-13 11:20:25 +0200

    added publish script

--- .buildkite/scripts/build-apple.sh
+++ .buildkite/scripts/build-apple.sh
@@ -14,7 +14,6 @@ fi
 add_bin_path
 with_go "${GOLANG_VERSION}"
 with_mage
-#google_cloud_auth
 
 retry 3 make -C "${makefile}" build GS_BUCKET_PATH=ingest-buildkite-ci
 docker images --format "table {{.Repository}}:{{.Tag}}\t{{.Size}}" --filter=reference="*/*/golang-crossbuild:llvm-apple*"
