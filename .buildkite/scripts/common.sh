50fefd4a25b5cebbbcc7c3ac275090f582d853f0
Olga Naidjonoka <olga_naidjonoka@epam.com>
2023-11-14 10:48:35 +0200

    changed crossbuild notification pattern and updated git-diff

--- .buildkite/scripts/common.sh
+++ .buildkite/scripts/common.sh
@@ -151,10 +151,11 @@ git_diff() {
   patterns=$1
   patterns_formatted="$(echo "$patterns" | tr ':' '\n')"
 
-  echo "Found files changed for $patterns:"
   if git diff --name-only HEAD@{1} HEAD | grep -qE -e "$patterns_formatted"; then
+    echo "Found files changed for $patterns..."
     echo true
   else
+    echo "No files changed for $patterns..."
     echo false
   fi
 }
