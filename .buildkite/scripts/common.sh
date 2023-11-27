3606052f1dab3f9c189c5662f42de0caf56b5563
Olga Naydyonock <olga.naidjonoka@elastic.co>
2023-11-27 20:01:21 +0200

    LLVM Apple & FPM pipelines migration (#330)
    (cherry picked from commit e2878ba7ffeab77cd3253167363b98d879cf73e9)
    
    # Conflicts:
    #	.buildkite/pipeline.yml

--- .buildkite/scripts/common.sh
+++ .buildkite/scripts/common.sh
@@ -146,3 +146,15 @@ check_is_arm() {
     is_arm=""
   fi
 }
+
+are_files_changed() {
+  changeset=$1
+
+  if git diff --name-only HEAD@{1} HEAD | grep -qE "$changeset"; then
+    return 0;
+  else
+    echo "WARN! No files changed in $changeset"
+    return 1;
+  fi
+}
+
