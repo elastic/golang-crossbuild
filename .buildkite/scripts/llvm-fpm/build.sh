ebbc3a676d76e8b4b231f950a8b908b40cb94a13
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2025-02-05 14:51:17 +0200

    fix adhoc changes

--- .buildkite/scripts/llvm-fpm/build.sh
+++ .buildkite/scripts/llvm-fpm/build.sh
@@ -16,5 +16,5 @@ add_bin_path
 with_go "${GOLANG_VERSION}"
 with_mage
 
-retry 3 make -C "${makefile}" build GS_BUCKET_PATH=$GS_BUCKET_PATH
+retry 3 make -C "${makefile}" build GS_BUCKET_PATH=ingest-buildkite-ci
 docker images --format "table {{.Repository}}:{{.Tag}}\t{{.Size}}" --filter=reference="${docker_filter_ref}"
