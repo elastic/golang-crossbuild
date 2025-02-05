c6363a5d0bf7d85a275a20f15344117fd0586a1f
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2025-02-05 09:45:44 +0000

    Changes to use libpcap 1.10.4

--- .buildkite/scripts/build.sh
+++ .buildkite/scripts/build.sh
@@ -11,9 +11,9 @@ check_is_arm
 add_bin_path
 with_go "${GOLANG_VERSION}"
 with_mage
-google_cloud_auth
 
-make -C go -f "${MAKEFILE}" build"${is_arm}" GS_BUCKET_PATH=ingest-buildkite-ci
+
+make -C go -f "${MAKEFILE}" build"${is_arm}" GS_BUCKET_PATH=$GS_BUCKET_PATH
 echo ":: List Docker images staging ::"
 docker images --format "table {{.Repository}}:{{.Tag}}\t{{.Size}}" --filter=reference="${STAGING_IMAGE}/golang-crossbuild"
 
