6426ed106a8061b1fbb3ac406307f148baa8cc31
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2025-03-03 20:10:46 +0200

    Revert "trigger fpm build -- to be removed"
    This reverts commit ea548d9901e74263c429e4cb16bdb5afc0dcbd0c.

--- .buildkite/scripts/llvm-fpm/publish.sh
+++ .buildkite/scripts/llvm-fpm/publish.sh
@@ -7,9 +7,9 @@ source .buildkite/scripts/common.sh
 makefile=${1}
 patterns=${2}
 
-#if ! are_files_changed "$patterns" ; then
-#    exit 0
-#fi
+if ! are_files_changed "$patterns" ; then
+    exit 0
+fi
 
 add_bin_path
 retry 3 make -C ${makefile} push
