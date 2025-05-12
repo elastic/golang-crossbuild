dfd3eaf922a4749a8159c21522ef5600ad71d63c
Olga Naydyonock <olga.naidjonoka@elastic.co>
2025-05-12 12:45:24 +0300

    test llvm-apple

--- .buildkite/scripts/common.sh
+++ .buildkite/scripts/common.sh
@@ -11,95 +11,95 @@ TMP_FOLDER="tmp.${REPO}"
 GOOGLE_CREDENTIALS_FILENAME="google-cloud-credentials.json"
 
 if [[ -z "${GOLANG_VERSION-""}" ]]; then
-    export GOLANG_VERSION=$(cat "${WORKSPACE}/.go-version")
+  export GOLANG_VERSION=$(cat "${WORKSPACE}/.go-version")
 fi
 
 add_bin_path() {
-    echo "Adding PATH to the environment variables..."
-    create_bin
-    export PATH="${PATH}:${BIN}"
+  echo "Adding PATH to the environment variables..."
+  create_bin
+  export PATH="${PATH}:${BIN}"
 }
 
 with_go() {
-    local go_version="${1}"
-    echo "Setting up the Go environment..."
-    create_bin
-    check_platform_architecture
-    retry 5 curl -sL -o ${BIN}/gvm "https://github.com/andrewkroh/gvm/releases/download/${SETUP_GVM_VERSION}/gvm-${PLATFORM_TYPE}-${arch_type}"
-    export PATH="${PATH}:${BIN}"
-    chmod +x ${BIN}/gvm
-    eval "$(gvm "$go_version")"
-    go version
-    which go
-    export PATH="${PATH}:$(go env GOPATH):$(go env GOPATH)/bin"
+  local go_version="${1}"
+  echo "Setting up the Go environment..."
+  create_bin
+  check_platform_architecture
+  retry 5 curl -sL -o ${BIN}/gvm "https://github.com/andrewkroh/gvm/releases/download/${SETUP_GVM_VERSION}/gvm-${PLATFORM_TYPE}-${arch_type}"
+  export PATH="${PATH}:${BIN}"
+  chmod +x ${BIN}/gvm
+  eval "$(gvm "$go_version")"
+  go version
+  which go
+  export PATH="${PATH}:$(go env GOPATH):$(go env GOPATH)/bin"
 }
 
 with_mage() {
-    local install_packages=(
-            "github.com/magefile/mage"
-            "github.com/elastic/go-licenser"
-            "golang.org/x/tools/cmd/goimports"
-            "github.com/jstemmer/go-junit-report"
-            "gotest.tools/gotestsum"
-    )
-    create_bin
-    for pkg in "${install_packages[@]}"; do
-        go install "${pkg}@latest"
-    done
+  local install_packages=(
+    "github.com/magefile/mage"
+    "github.com/elastic/go-licenser"
+    "golang.org/x/tools/cmd/goimports"
+    "github.com/jstemmer/go-junit-report"
+    "gotest.tools/gotestsum"
+  )
+  create_bin
+  for pkg in "${install_packages[@]}"; do
+    go install "${pkg}@latest"
+  done
 }
 
 create_bin() {
-    if [[ ! -d "${BIN}" ]]; then
+  if [[ ! -d "${BIN}" ]]; then
     mkdir -p ${BIN}
-    fi
+  fi
 }
 
 check_platform_architecture() {
-# for downloading the GVM and Terraform packages
+  # for downloading the GVM and Terraform packages
   case "${HW_TYPE}" in
-   "x86_64")
-        arch_type="amd64"
-        ;;
-    "aarch64")
-        arch_type="arm64"
-        ;;
-    "arm64")
-        arch_type="arm64"
-        ;;
-    *)
+  "x86_64")
+    arch_type="amd64"
+    ;;
+  "aarch64")
+    arch_type="arm64"
+    ;;
+  "arm64")
+    arch_type="arm64"
+    ;;
+  *)
     echo "The current platform/OS type is unsupported yet"
     ;;
   esac
 }
 
 retry() {
-    local retries=$1
-    shift
-    local count=0
-    until "$@"; do
-        exit=$?
-        wait=$((2 ** count))
-        count=$((count + 1))
-        if [ $count -lt "$retries" ]; then
-            >&2 echo "Retry $count/$retries exited $exit, retrying in $wait seconds..."
-            sleep $wait
-        else
-            >&2 echo "Retry $count/$retries exited $exit, no more retries left."
-            return $exit
-        fi
-    done
-    return 0
+  local retries=$1
+  shift
+  local count=0
+  until "$@"; do
+    exit=$?
+    wait=$((2 ** count))
+    count=$((count + 1))
+    if [ $count -lt "$retries" ]; then
+      >&2 echo "Retry $count/$retries exited $exit, retrying in $wait seconds..."
+      sleep $wait
+    else
+      >&2 echo "Retry $count/$retries exited $exit, no more retries left."
+      return $exit
+    fi
+  done
+  return 0
 }
 
 google_cloud_auth() {
-    local gsUtilLocation=$(mktemp -d -p ${BIN} -t "${TMP_FOLDER}.XXXXXXXXX")
-    local secretFileLocation=${gsUtilLocation}/${GOOGLE_CREDENTIALS_FILENAME}
-    echo "${PRIVATE_CI_GCS_CREDENTIALS_SECRET}" > ${secretFileLocation}
-    gcloud auth activate-service-account --key-file ${secretFileLocation} 2> /dev/null
-    export GOOGLE_APPLICATION_CREDENTIALS=${secretFileLocation}
+  local gsUtilLocation=$(mktemp -d -p ${BIN} -t "${TMP_FOLDER}.XXXXXXXXX")
+  local secretFileLocation=${gsUtilLocation}/${GOOGLE_CREDENTIALS_FILENAME}
+  echo "${PRIVATE_CI_GCS_CREDENTIALS_SECRET}" >${secretFileLocation}
+  gcloud auth activate-service-account --key-file ${secretFileLocation} 2>/dev/null
+  export GOOGLE_APPLICATION_CREDENTIALS=${secretFileLocation}
 }
 
-unset_secrets () {
+unset_secrets() {
   for var in $(printenv | sed 's;=.*;;' | sort); do
     if [[ "$var" == *_SECRET || "$var" == *_TOKEN ]]; then
       unset "$var"
@@ -111,7 +111,7 @@ google_cloud_logout_active_account() {
   local active_account=$(gcloud auth list --filter=status:ACTIVE --format="value(account)" 2>/dev/null)
   if [[ -n "$active_account" && -n "${GOOGLE_APPLICATION_CREDENTIALS+x}" ]]; then
     echo "Logging out from GCP for active account"
-    gcloud auth revoke $active_account > /dev/null 2>&1
+    gcloud auth revoke $active_account >/dev/null 2>&1
   else
     echo "No active GCP accounts found."
   fi
@@ -150,11 +150,15 @@ check_is_arm() {
 are_files_changed() {
   changeset=$1
 
+  gitDiff=$(git diff --name-only HEAD@{1} HEAD)
+  whatChanged=$(git diff --name-only HEAD@{1} HEAD | grep -qE "$changeset")
+  echo "--- Git Diff: $gitDiff"
+  echo "--- Changed files: $whatChanged"
+
   if git diff --name-only HEAD@{1} HEAD | grep -qE "$changeset"; then
-    return 0;
+    return 0
   else
     echo "WARN! No files changed in $changeset"
-    return 1;
+    return 1
   fi
 }
-
