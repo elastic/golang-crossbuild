fc6710cede1765128672f399909cc3299a5be8f6
Victor Martinez <VictorMartinezRubio@gmail.com>
2025-05-13 02:09:08 +0200

    fix google bucket access

--- .buildkite/scripts/llvm-apple/build.sh
+++ .buildkite/scripts/llvm-apple/build.sh
@@ -9,6 +9,7 @@ makefile=${1}
 add_bin_path
 with_go "${GOLANG_VERSION}"
 with_mage
+google_cloud_auth
 
 retry 3 make -C "${makefile}" build GS_BUCKET_PATH=ingest-buildkite-ci
 docker images --format "table {{.Repository}}:{{.Tag}}\t{{.Size}}"
