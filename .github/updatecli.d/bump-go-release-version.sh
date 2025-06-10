15d0e7f017d1f4f9f1edb81db56ccb3c05a0debe
Victor Martinez <victormartinezrubio@gmail.com>
2025-06-10 15:59:46 +0200

    validate installed golang version and honour VERSION build-args (#625)
    (cherry picked from commit 416d57c76a2c1a822f9cbd033826bea169a4693e)
    
    # Conflicts:
    #	.github/updatecli.d/bump-microsoft.yml
    #	go/base-arm/Dockerfile.tmpl
    #	go/base/Dockerfile.tmpl

--- .github/updatecli.d/bump-go-release-version.sh
+++ .github/updatecli.d/bump-go-release-version.sh
@@ -43,7 +43,6 @@ echo "Update go version ${GO_RELEASE_VERSION}"
 
 find "go" -type f -name Dockerfile.tmpl -print0 |
     while IFS= read -r -d '' line; do
-        ${SED} -E -e "s#(ARG GOLANG_VERSION)=[0-9]+\.[0-9]+(\.[0-9]+)?#\1=${GO_RELEASE_VERSION}#g" "$line"
         if echo "$line" | grep -q 'arm' ; then
             ${SED} -E -e "s#(ARG GOLANG_DOWNLOAD_SHA256)=.+#\1=${GOLANG_DOWNLOAD_SHA256_ARM}#g" "$line"
             ${SED} -E -e "s#(ARG MSFT_DOWNLOAD_SHA256)=.+#\1=${MSFT_DOWNLOAD_SHA256_ARM}#g" "$line"
