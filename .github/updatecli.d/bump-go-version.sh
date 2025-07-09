1b40a6dd4642b917e6b7638ab82d6fbe9bb86e21
Victor Martinez <victormartinezrubio@gmail.com>
2025-07-09 22:45:34 +0200

    updatecli: converge both golang versions together (#634)
    Co-authored-by: Dimitrios Liappis <dimitrios.liappis@gmail.com>

--- .github/updatecli.d/bump-go-version.sh
+++ .github/updatecli.d/bump-go-version.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 #
-# Given the Golang release version this script will bump the version.
+# Given the Microsoft Golang release version this script will bump the version.
 #
 # This script is executed by the automation we are putting in place
 #
-# NOTE: sha256 is retrieved from https://pkg.go.dev/golang.org/x/website/internal/dl?utm_source=godoc
+# NOTE: 
+#   * sha256 is retrieved from https://pkg.go.dev/golang.org/x/website/internal/dl?utm_source=godoc
+#   * sha256 is retrieved from https://aka.ms/golang/release/latest/go${MAJOR_MINOR_PATCH_VERSION}.assets.json
 #
 # Parameters:
-#	$1 -> the Golang release version to be bumped. Mandatory.
+#	$1 -> the Microsoft Golang release version to be bumped. Mandatory.
 #
 set -euo pipefail
 MSG="parameter missing."
@@ -19,33 +21,29 @@ else
 	SED="sed -i"
 fi
 
-MAJOR_MINOR_VERSION=$(echo "$GO_RELEASE_VERSION" | sed -E -e "s#([0-9]+\.[0-9]+).*#\1#g")
+# Process the GO_RELEASE_VERSION to extract major, minor, patch and security versions
+MAJOR_MINOR_PATCH_VERSION=${GO_RELEASE_VERSION%-*}
+SECURITY_VERSION="-${GO_RELEASE_VERSION##*-}"
+
+# Gather golang/go sha256 values
 GOLANG_DOWNLOAD_SHA256_ARM=$(curl -s -L https://golang.org/dl/\?mode\=json | jq -r ".[] | select( .version | contains(\"go${GO_RELEASE_VERSION}\")) | .files[] | select (.filename | contains(\"go${GO_RELEASE_VERSION}.linux-arm64.tar.gz\")) | .sha256")
 GOLANG_DOWNLOAD_SHA256_AMD=$(curl -s -L https://golang.org/dl/\?mode\=json | jq -r ".[] | select( .version | contains(\"go${GO_RELEASE_VERSION}\")) | .files[] | select (.filename | contains(\"go${GO_RELEASE_VERSION}.linux-amd64.tar.gz\")) | .sha256")
 
 # Gather microsoft/go sha256 values
-URL=https://aka.ms/golang/release/latest/go${GO_RELEASE_VERSION}.assets.json
-if curl -s -L "$URL" > /dev/null ; then
-    MSFT_DOWNLOAD_METADATA=$(curl -s -L "$URL")
-    MSFT_DOWNLOAD_SHA256_ARM=$(echo $MSFT_DOWNLOAD_METADATA | jq -r ".arches[] | select( .env.GOOS == \"linux\") | select( .env.GOARCH == \"arm64\") | .sha256")
-    MSFT_DOWNLOAD_SHA256_AMD=$(echo $MSFT_DOWNLOAD_METADATA | jq -r ".arches[] | select( .env.GOOS == \"linux\") | select( .env.GOARCH == \"amd64\") | .sha256")
-    # when a new minor then we use `-1`
-    # then nex security versions will be `-2`, `-3`, etc, see bump-microsoft.yml
-    SECURITY_VERSION=-1
-fi
+MSFT_DOWNLOAD_METADATA=$(curl -s -L https://aka.ms/golang/release/latest/go${MAJOR_MINOR_PATCH_VERSION}.assets.json)
+MSFT_DOWNLOAD_SHA256_ARM=$(echo $MSFT_DOWNLOAD_METADATA | jq -r ".arches[] | select( .env.GOOS == \"linux\") | select( .env.GOARCH == \"arm64\") | .sha256")
+MSFT_DOWNLOAD_SHA256_AMD=$(echo $MSFT_DOWNLOAD_METADATA | jq -r ".arches[] | select( .env.GOOS == \"linux\") | select( .env.GOARCH == \"amd64\") | .sha256")
 
 ## As long as https://golang.org/dl/?mode=json supports only 2 major versions
 ## and there is a new major release, then it's required to parse https://golang.org/dl
 ## see https://github.com/elastic/golang-crossbuild/pull/389/commits/d0af04f97a2381630ea5e8da5a99f50cf27856a0
 if [ -z "$GOLANG_DOWNLOAD_SHA256_ARM" ] ; then
-    GOLANG_DOWNLOAD_SHA256_ARM=$(curl -s -L https://golang.org/dl | grep go${GO_RELEASE_VERSION}.linux-arm64.tar.gz -A 5 | grep "<tt>" | sed 's#.*<tt>##g' | sed 's#</t.*##g')
+    GOLANG_DOWNLOAD_SHA256_ARM=$(curl -s -L https://golang.org/dl | grep "go${MAJOR_MINOR_PATCH_VERSION}.linux-arm64.tar.gz" -A 5 | grep "<tt>" | sed 's#.*<tt>##g' | sed 's#</t.*##g')
 fi
 
 if [ -z "$GOLANG_DOWNLOAD_SHA256_AMD" ] ; then
-    GOLANG_DOWNLOAD_SHA256_AMD=$(curl -s -L https://golang.org/dl | grep go${GO_RELEASE_VERSION}.linux-amd64.tar.gz -A 5 | grep "<tt>" | sed 's#.*<tt>##g' | sed 's#</t.*##g')
+    GOLANG_DOWNLOAD_SHA256_AMD=$(curl -s -L https://golang.org/dl | grep "go${MAJOR_MINOR_PATCH_VERSION}.linux-amd64.tar.gz" -A 5 | grep "<tt>" | sed 's#.*<tt>##g' | sed 's#</t.*##g')
 fi
-
-echo "Update go version ${GO_RELEASE_VERSION}"
 
 find "go" -type f -name Dockerfile.tmpl -print0 |
     while IFS= read -r -d '' line; do
@@ -64,3 +62,11 @@ find "go" -type f -name Dockerfile.tmpl -print0 |
             ${SED} -E -e "s#(ARG SECURITY_VERSION)=.*#\1=${SECURITY_VERSION}#g" "$line"
         fi
     done
+
+if git diff --quiet ; then
+    # No modifications – exit successfully but keep stdout empty to that updatecli is happy
+    exit 0
+else
+    echo "Update Go version ${GO_RELEASE_VERSION}"
+    git --no-pager diff
+fi
