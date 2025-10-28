6d4a9cf09dd60e261267ce229f1d811ab4956dff
Victor Martinez <victormartinezrubio@gmail.com>
2025-10-28 09:48:51 +0100

    release: llvm (#676)

--- .buildkite/scripts/llvm-apple/build.sh
+++ .buildkite/scripts/llvm-apple/build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+# This script builds the Docker images but does not push them to a registry.
+# Env variables:
+# - RELEASE: If set, indicates that this is a production release and images should be name to production. For manual releases
+# - GOLANG_VERSION: Version of Go to use for the build. Defaults to the value in .buildkite/pipeline.yml.
+# - MAKEFILE: Path to the Makefile to use.
+# - STAGING_IMAGE: Docker repository to use for staging images.
 
 set -euo pipefail
 
@@ -9,6 +15,11 @@ makefile=${1}
 add_bin_path
 with_go "${GOLANG_VERSION}"
 with_mage
+
+# if RELEASE is not set then set REPOSITORY to STAGING_IMAGE
+if [[ -z "${RELEASE:-}" ]]; then
+  export REPOSITORY="${STAGING_IMAGE}"
+fi
 
 retry 3 make -C "${makefile}" build GS_BUCKET_PATH=golang-crossbuild-ci-internal
 
--- .buildkite/scripts/llvm-apple/publish.sh
+++ .buildkite/scripts/llvm-apple/publish.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+# Env variables:
+# - RELEASE: If set, indicates that this is a production release and images should be pushed to production. For manual releases
+# - MAKEFILE: Path to the Makefile to use.
+# - STAGING_IMAGE: Docker repository to use for staging images.
 
 set -euo pipefail
 
@@ -7,4 +11,10 @@ source .buildkite/scripts/common.sh
 makefile=${1}
 
 add_bin_path
+
+# if RELEASE is not set then set REPOSITORY to STAGING_IMAGE
+if [[ -z "${RELEASE:-}" ]]; then
+  export REPOSITORY="${STAGING_IMAGE}"
+fi
+
 retry 3 make -C ${makefile} push
