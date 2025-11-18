5989eea3c4907c883e6b7f3338f0f52bd2262bb4
fr4nc1sc0.r4m0n <francisco.ramon@elastic.co>
2025-11-18 19:49:13 +0100

    Try to check python installation

--- .buildkite/scripts/build.sh
+++ .buildkite/scripts/build.sh
@@ -12,6 +12,8 @@ add_bin_path
 with_go "${GOLANG_VERSION}"
 with_mage
 
+echo $(which python3.10)
+
 make -C go -f "${MAKEFILE}" build"${is_arm}" GS_BUCKET_PATH=golang-crossbuild-ci-internal
 
 echo "--- List Docker images staging"
