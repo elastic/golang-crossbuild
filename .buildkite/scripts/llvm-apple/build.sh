436eee16d01ad8b499f069f58c2cd674d0197d6d
fr4nc1sc0.r4m0n <francisco.ramon@elastic.co>
2025-11-18 19:54:30 +0100

    Fix command to check python installation

--- .buildkite/scripts/llvm-apple/build.sh
+++ .buildkite/scripts/llvm-apple/build.sh
@@ -21,6 +21,9 @@ if [[ -z "${RELEASE:-}" ]]; then
   export REPOSITORY="${STAGING_IMAGE}"
 fi
 
+echo "Checking python installation"
+echo $(which python3.10)
+
 retry 3 make -C "${makefile}" build GS_BUCKET_PATH=golang-crossbuild-ci-internal
 
 echo "--- List Docker images"
