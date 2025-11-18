1395b466c278779358cf218dd2cfbe8c243a4e0c
fr4nc1sc0.r4m0n <francisco.ramon@elastic.co>
2025-11-18 19:49:29 +0100

    Try to check python installation

--- .buildkite/scripts/build.sh
+++ .buildkite/scripts/build.sh
@@ -12,6 +12,7 @@ add_bin_path
 with_go "${GOLANG_VERSION}"
 with_mage
 
+echo "Checking python installation"
 echo $(which python3.10)
 
 make -C go -f "${MAKEFILE}" build"${is_arm}" GS_BUCKET_PATH=golang-crossbuild-ci-internal
